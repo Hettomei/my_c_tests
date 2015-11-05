11d326f0a56375c5183e5d0c94ab4a3a1484e6e1
Hettomei <itsumo.sibyllin@gmail.com>
2015-11-05 22:44:26 +0100

    initial commit

---
+++ argument_depart.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+/*	
+	argv[0] contiendra ./exo
+
+	argv[1] contiendra un
+
+	argv[2] contiendra deux
+
+	argv[3] contiendra trois
+
+	argv[4] contiendra quatre
+	*/
+	
+	printf("\n\n%s\n%s\n%s\n\n",argv[0], argv[1],argv[2]);
+	
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# compile
+gcc argument_depart.c -o argument_depart
+
+# run
+./argument_depart arg1 arg2
